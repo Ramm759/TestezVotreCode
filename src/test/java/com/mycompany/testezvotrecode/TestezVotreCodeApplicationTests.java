45d62e8bab96edf597ab02b78eeecb26e30b07e3
Ramm759 <fredgsn59@gmail.com>
2022-01-20 20:15:27 +0100

    Initial commit

---
+++ src/test/java/com/mycompany/testezvotrecode/TestezVotreCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.testezvotrecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestezVotreCodeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
